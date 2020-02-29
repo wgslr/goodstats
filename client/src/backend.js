58bbbf6c23d5171c883c121a3f71f2c1f5ef12b4
Wojciech Geisler <wojciech.geisler@gmail.com>
2020-03-01 00:50:57 +0100

    client: Use the Fetch API for requests

--- client/src/backend.js
+++ client/src/backend.js
@@ -1,38 +1,29 @@
 const SERVER = 'localhost:3001'
 
+export async function listShelvedBooks(userId) {
+  const xmlDoc = await get('/review/list', { id: userId });
+  var snapshot = xmlDoc.evaluate('//reviews/review/book/title',
+    xmlDoc.documentElement, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null
+  );
+  const titles = [];
+  for (let i = 0; i < snapshot.snapshotLength; ++i) {
+    const item = snapshot.snapshotItem(i);
+    titles.push(item.textContent);
+  }
 
-export function listShelvedBooks(userId) {
-  return new Promise((resolve, reject) => {
-    console.log("Entered listReviews");
-    const xhr = new XMLHttpRequest();
-    const URL = `http://${SERVER}/goodreads/review/list?id=${userId}`;
+  return titles;
+}
 
-    const titles = [];
 
-    xhr.open("GET", URL, true);
-    xhr.onload = function (e) {
-      if (xhr.readyState === 4) {
-        if (xhr.status === 200) {
-          const xml = xhr.responseXML;
-          var snapshot = xml.evaluate('//reviews/review/book/title',
-            xml.documentElement, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null
-          );
-          for (let i = 0; i < snapshot.snapshotLength; ++i) {
-            const item = snapshot.snapshotItem(i);
-            titles.push(item.textContent);
-          }
-
-          resolve(titles)
-
-        } else {
-          reject(`Server returned ${xhr.status} ${xhr.statusText} response`)
-        }
-      }
-    };
-    xhr.onerror = function (e) {
-      console.error(xhr.statusText);
-      reject(e)
-    };
-    xhr.send(null);
-  });
+async function get(path, queryParams) {
+  const url = new URL(`http://${SERVER}/goodreads/${path}`);
+  url.search = new URLSearchParams(queryParams).toString();
+  const response = await window.fetch(url, { mehod: 'GET', })
+  if (!response.ok) {
+    throw (`Server returned ${response.status} ${response.statusText} response`)
+  }
+  const body = await response.text();
+  const doc = (new DOMParser()).parseFromString(body, 'application/xml');
+  console.log("Parsed as ", doc)
+  return doc;
 }
