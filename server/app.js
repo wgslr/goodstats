95318e8139a618284b9825ce6bf1ccd1e5229db6
Wojciech Geisler <wojciech.geisler@gmail.com>
2020-02-29 20:47:32 +0100

    server: Correctly read whole response stream

--- server/app.js
+++ server/app.js
@@ -10,41 +10,48 @@ const app = express();
 const port = 3001;
 
 
-function proxyRequest(req, callback) {
-  const { method, params: { path } } = req
-  let reqPath = basePath + path + '?';
-  for (let queryParam in req.query) {
-    // TODO sanitize
-    reqPath += `&${queryParam}=${req.query[queryParam]}`;
-  }
-  reqPath += '&key=' + apiKey+ '&v=2';
-  
-  console.log("path: ", reqPath);
-  const options = {
-    'hostname': GoodReadsHostname,
-    'path': reqPath,
-    'method': method,
-  }
+function proxyRequest(req) {
+  return new Promise((resolve, reject) => {
+    const { method, params: { path } } = req
+    let reqPath = basePath + path + '?';
+    for (let queryParam in req.query) {
+      // TODO sanitize
+      reqPath += `&${queryParam}=${req.query[queryParam]}`;
+    }
+    reqPath += '&key=' + apiKey+ '&v=2';
+    
+    console.log("path: ", reqPath);
+    const options = {
+      'hostname': GoodReadsHostname,
+      'path': reqPath,
+      'method': method,
+    }
 
-  const proxyReq = https.request(options, (res) => {
-    // console.log("res: ", res.url);
-    console.log('statusCode:', res.statusCode);
-    console.log('headers:', res.headers);
+    const proxyReq = https.request(options, (res) => {
+      // console.log("res: ", res.url);
+      console.log('statusCode:', res.statusCode);
+      console.log('headers:', res.headers);
 
-    res.on('data', (d) => {
-      callback(d)
+      let body = '';
+
+      res.on('data', (d) => {
+        body += d;
+      });
+      res.on('end', () => {
+        resolve(body);
+      });
     });
-  });
 
-  proxyReq.on('error', (e) => {console.log("error: ", e)});
-  proxyReq.end();
+    proxyReq.on('error', reject);
+    proxyReq.end();
+
+  })
 }
 
-app.all('/goodreads/:path(*)', (req, res) => {
-  proxyRequest(req, (data) =>  {
-    console.log('data', data);
-    res.send(data)
-  });
+app.all('/goodreads/:path(*)', async (req, res) => {
+  const result = await proxyRequest(req)
+  // console.log('result', result);
+  res.send(result)
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
