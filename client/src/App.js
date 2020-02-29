2d41047756133ce5abd248179adc38562b7be0bc
Wojciech Geisler <wojciech.geisler@gmail.com>
2020-03-01 00:33:15 +0100

    client: Move data fetching to separate module

--- client/src/App.js
+++ client/src/App.js
@@ -1,46 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
+import { listShelvedBooks } from './backend.js'
 import './App.css';
 
-const SERVER = 'localhost:3001'
-const USER_ID = '29690543';
-
-function listReviewedBooks(userId) {
-  return new Promise((resolve, reject) => {
-    console.log("Entered listReviews");
-    const xhr = new XMLHttpRequest();
-    const URL = `http://${SERVER}/goodreads/review/list?id=${userId}`;
-
-    const titles = [];
-
-    xhr.open("GET", URL, true);
-    xhr.onload = function (e) {
-      if (xhr.readyState === 4) {
-        if (xhr.status === 200) {
-          const xml = xhr.responseXML;
-          var snapshot = xml.evaluate('//reviews/review/book/title',
-            xml.documentElement, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null
-          );
-          for (let i = 0; i < snapshot.snapshotLength; ++i) {
-            const item = snapshot.snapshotItem(i);
-            titles.push(item.textContent);
-          }
-
-          resolve(titles)
-
-        } else {
-          reject(`Server returned ${xhr.status} ${xhr.statusText} response`)
-        }
-      }
-    };
-    xhr.onerror = function (e) {
-      console.error(xhr.statusText);
-      reject(e)
-    };
-    xhr.send(null);
-  });
-}
-
+const DEFAULT_USER_ID = '29690543';
 
 const STATUS = {
   'LOADING': 'loading',
@@ -57,7 +20,7 @@ function TitlesList() {
     details: {},
   });
 
-  const [userId, setUserId] = useState(USER_ID);
+  const [userId, setUserId] = useState(DEFAULT_USER_ID);
 
   useEffect(() => {
     setState(prev => ({
@@ -65,7 +28,7 @@ function TitlesList() {
       status: STATUS.LOADING,
       details: {}
     }))
-    listReviewedBooks(userId).then(
+    listShelvedBooks(userId).then(
       titles => setState(prev => ({
         ...prev,
         status: STATUS.LOADED,
