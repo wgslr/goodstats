f993dd6a0487ef739d5720bc1279020c33f2d061
Wojciech Geisler <wojciech.geisler@gmail.com>
2020-02-29 22:25:23 +0100

    Remove debug logs

--- client/src/App.js
+++ client/src/App.js
@@ -18,26 +18,15 @@ function listReviewedBooks() {
       console.log("onload", e)
       if (xhr.readyState === 4) {
         if (xhr.status === 200) {
-          console.log("parsing")
-
-          console.log('responseText', xhr.responseText);
           const xml = xhr.responseXML;
-          console.log('xml', xml);
-
-          var snapshot = xml.evaluate('//reviews/review/book/title', xml.documentElement, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null );
-          console.log("Snaphsot: ", snapshot);
-          for(let i = 0; i < snapshot.snapshotLength; ++i) {
+          var snapshot = xml.evaluate('//reviews/review/book/title',
+            xml.documentElement, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null
+          );
+          for (let i = 0; i < snapshot.snapshotLength; ++i) {
             const item = snapshot.snapshotItem(i);
-            console.log('item', item);
             titles.push(item.textContent);
           }
-          
-          // while (thisTitle) {
-          //   console.log(s)
-          //   titles.push(thisTitle.textContent)
-          //   thisTitle = titleIterator.iterateNext();
-          // }	
-          console.log("resolving with ", titles)
+
           resolve(titles)
 
         } else {
