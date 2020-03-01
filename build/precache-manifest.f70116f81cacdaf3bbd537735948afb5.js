f2b748b2a1fa5ae0c0786eb04cd540865a7715e3
Wojciech Geisler <wojciech.geisler@gmail.com>
2020-03-01 17:56:28 +0100

    Add client files

---
+++ build/precache-manifest.f70116f81cacdaf3bbd537735948afb5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e6ee22c67bc41c55afd27f5f439d1b4",
+    "url": "/goodstats/index.html"
+  },
+  {
+    "revision": "997ef520b5f641f8c146",
+    "url": "/goodstats/static/css/main.683a47ee.chunk.css"
+  },
+  {
+    "revision": "ae5a0e0686dadd06fbf7",
+    "url": "/goodstats/static/js/2.14c74b11.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/goodstats/static/js/2.14c74b11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "997ef520b5f641f8c146",
+    "url": "/goodstats/static/js/main.ffbabe31.chunk.js"
+  },
+  {
+    "revision": "1624731d493779158a94",
+    "url": "/goodstats/static/js/runtime-main.f25abf1f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/goodstats/static/media/logo.5d5d9eef.svg"
+  }
+]);
