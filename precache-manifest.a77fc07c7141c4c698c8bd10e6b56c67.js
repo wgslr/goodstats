6f60cb317c44a914833d96245534bea6bdbddf60
Wojciech Geisler <wojciech.geisler@gmail.com>
2020-03-01 18:15:50 +0100

    Change server domain

---
+++ precache-manifest.a77fc07c7141c4c698c8bd10e6b56c67.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d96ca4daf405c30cf4035dbf913ea66",
+    "url": "/goodstats/index.html"
+  },
+  {
+    "revision": "b7d9ecd18f4a1c327e13",
+    "url": "/goodstats/static/css/main.683a47ee.chunk.css"
+  },
+  {
+    "revision": "ae5a0e0686dadd06fbf7",
+    "url": "/goodstats/static/js/2.14c74b11.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/goodstats/static/js/2.14c74b11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7d9ecd18f4a1c327e13",
+    "url": "/goodstats/static/js/main.3a25111d.chunk.js"
+  },
+  {
+    "revision": "1624731d493779158a94",
+    "url": "/goodstats/static/js/runtime-main.f25abf1f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/goodstats/static/media/logo.5d5d9eef.svg"
+  }
+]);
